008ced849880461867319bb6798b2513940eee1b
fcmutuc <francis.mutuc@gmail.com>
2022-03-14 10:25:56 -0400

    Add component for app instructions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import Instructions from '../Instructions/Instructions.js';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <h2>Rectangles App</h2>
+      <Instructions />
     </div>
   );
 }
---
+++ src/components/Instructions/Instructions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Instructions.css';
+
+const Instructions = () => (
+  <div className="instructions">
+    <p>Enter instructions here.</p>
+  </div>
+)
+
+export default Instructions;
