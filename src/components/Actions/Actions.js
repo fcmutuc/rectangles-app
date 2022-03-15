ddd1cbcc888ec71ff485965b6be76325dcc0a013
fcmutuc <francis.mutuc@gmail.com>
2022-03-14 23:53:11 -0400

    Add initial handler for clicking action icons

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -3,13 +3,17 @@ import Stack from '@mui/material/Stack';
 import CropLandscapeIcon from '@mui/icons-material/CropLandscape';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+const handleIconClicks = name => () => {
+  console.log(name);
+}
+
 export default function Actions() {
   return (
-    <Stack direction="row" spacing={1}>
-      <IconButton aria-label="add an rectangle">
+    <Stack className="icons" direction="row" spacing={1}>
+      <IconButton name="add-rectangle" aria-label="add a rectangle" onClick={handleIconClicks('add')}>
         <CropLandscapeIcon />
       </IconButton>
-      <IconButton aria-label="delete">
+      <IconButton name="clear-canvas" aria-label="clear the canvas" onClick={handleIconClicks('clear')}>
         <DeleteIcon />
       </IconButton>
     </Stack>
