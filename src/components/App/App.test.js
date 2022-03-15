e73a49e6dcf2ebafd20fbb49a278b03391b8aa8f
fcmutuc <francis.mutuc@gmail.com>
2022-03-15 08:30:33 -0400

    Fix failing unit test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders instructions text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/You can add up to two rectangles to the drawing canvas, the area inside the box below./i);
   expect(linkElement).toBeInTheDocument();
 });
