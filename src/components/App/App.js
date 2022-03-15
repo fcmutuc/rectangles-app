6a66cf887d9dbbbf857518873a05e3ed76533c03
fcmutuc <francis.mutuc@gmail.com>
2022-03-15 02:53:06 -0400

    Add up to 2 rectangles and also clear the canvas

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 import Instructions from '../Instructions/Instructions.js';
-import Actions from '../Actions/Actions.js';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import CropLandscapeIcon from '@mui/icons-material/CropLandscape';
+import DeleteIcon from '@mui/icons-material/Delete';
 import './App.css';
 
-function App() {
+export default function App() {
+  const { editor, onReady } = useFabricJSEditor();
+
+  const [rectangleCount, setRectangleCount] = useState(0);
+
+  const onAddRectangle = () => {
+    if (rectangleCount === 2) {
+      alert('Only 2 Rectangles are currently allowed.');
+      return false;
+    }
+    if (rectangleCount >= 0 || rectangleCount < 2) {
+      setRectangleCount(rectangleCount + 1);
+      editor.addRectangle();
+    }
+  };
+
+  const onClearCanvas = () => {
+    editor.deleteAll();
+    setRectangleCount(0);
+  };
+
   return (
-    <div className="container">
-      <h2>Rectangles App</h2>
+    <div className="App">
+      <h1>Rectangles App</h1>
       <Instructions />
-      <Actions />
+      <Stack direction="row" spacing={1}>
+        <IconButton name="add-rectangle" aria-label="add a rectangle" onClick={onAddRectangle}>
+          <CropLandscapeIcon />
+        </IconButton>
+        <IconButton name="clear-canvas" aria-label="clear the canvas" onClick={onClearCanvas}>
+          <DeleteIcon />
+        </IconButton>
+      </Stack>
+      <FabricJSCanvas className="sample-canvas" onReady={onReady} />
     </div>
   );
 }
-
-export default App;
