fe0aebb35f81fb02d8dcedec2dfc6f1e4e6d9aa0
fcmutuc <francis.mutuc@gmail.com>
2022-03-15 07:20:36 -0400

    Touch up instructions; add tooltip to icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 import Instructions from '../Instructions/Instructions.js';
+import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import CropLandscapeIcon from '@mui/icons-material/CropLandscape';
@@ -10,7 +11,6 @@ import './App.css';
 export default function App() {
   const { selectedObjects, editor, onReady } = useFabricJSEditor();
   const [rectangleCount, setRectangleCount] = useState(0);
-  //const [objectIsHighlighted, setObjectIsHighlighted] = useState(false);
 
   const onAddRectangle = () => {
     if (rectangleCount === 2) {
@@ -37,6 +37,20 @@ export default function App() {
   };
 
   useEffect(() => {
+
+    const printCoordinates = (options) => {
+      console.log('in printCoordinates()');
+      console.log('options ->');
+      console.log(options);
+      options.target.setCoords();
+      editor.canvas.forEachObject(function(obj) {
+        if (obj === options.target) {
+            return;
+        }
+        obj.set('opacity' ,options.target.intersectsWithObject(obj) ? 0.5 : 1);
+      });
+    };
+
     console.log('in useEffect');
     if (selectedObjects !== undefined) {
       console.log('selectedObjects ->');
@@ -45,6 +59,11 @@ export default function App() {
     if (editor?.canvas !== undefined) {
       console.log('editor.canvas.getActiveObjects() ->');
       console.log(editor.canvas.getActiveObjects());
+      editor.canvas.on({
+        'object:moving': printCoordinates,
+        'object:scaling': printCoordinates,
+        'object:rotating': printCoordinates
+      });
     }
   }, [rectangleCount, editor?.canvas, selectedObjects])
 
@@ -53,12 +72,16 @@ export default function App() {
       <h1>Rectangles App</h1>
       <Instructions />
       <Stack direction="row" spacing={1}>
+      <Tooltip title="Add a Rectangle">
         <IconButton name="add-rectangle" aria-label="add a rectangle" onClick={onAddRectangle}>
           <CropLandscapeIcon />
         </IconButton>
+        </Tooltip>
+        <Tooltip title="Clear the Canvas">
         <IconButton name="clear-canvas" aria-label="clear the canvas" onClick={onClearCanvas}>
           <DeleteIcon />
         </IconButton>
+        </Tooltip>
       </Stack>
       <FabricJSCanvas className="sample-canvas" onReady={onReady} />
     </div>
--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -3,7 +3,21 @@ import './Instructions.css';
 
 const Instructions = () => (
   <div className="instructions">
-    <p>Enter instructions here.</p>
+    <p>
+      You can add up to two rectangles to the drawing canvas, the area inside the box below.<br/><br/>
+      When there are two rectangles on the drawing canvas, you can see if the following:
+    </p>
+    <ul>
+      <li>
+        the two rectangles intersect
+      </li>
+      <li>
+        one of the rectangles is wholly contained within another rectangle
+      </li>
+      <li>
+        the two rectangles are adjacent
+      </li>
+    </ul>
   </div>
 )
 
